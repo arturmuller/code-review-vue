3f1c354fbba8243bd7f71f1edd089bccf537cf74
Artur Müller <me@arturmuller.com>
2025-02-12 14:57:09 +0100

    Add docs to slowFetch

--- src/utils/slowFetch.ts
+++ src/utils/slowFetch.ts
@@ -2,6 +2,12 @@ export function wait(ms: number) {
   return new Promise((r) => setTimeout(r, ms));
 }
 
+/**
+ * This function is identical to window.fetch, only it waits for a bit before
+ * returning to simulate API request with latency.
+ *
+ * You can ignore it for the purposes of the pair programming session.
+ */
 export async function fetch(input: string | URL, init?: RequestInit) {
   await wait(550);
   return window.fetch(input, init);
